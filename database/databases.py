82a8537cc7f7226924e8192ce57a4885caa0bd71
Anurag_kumar <anuragkumar777x@gmail.com>
2026-01-16 12:59:54 +0530

    Update databases.py

--- database/databases.py
+++ database/databases.py
@@ -1,6 +1,8 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-db_url = "postgresql://postgres:AnuragKumar%40123@localhost:5432/myDB"
-engine = create_engine(db_url)
-session = sessionmaker(autocommit = False, autoflush=False, bind=engine)
 
+db_url = os.getenv("DATABASE_URL")
+
+engine = create_engine(db_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
